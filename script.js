c6dbac956532872900bfb697329e9981627f7f06
raunak5131 <raunakvernekar19@gmail.com>
2025-06-22 19:18:01 +0530

    Made navbar responsive with media queries

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("toggle-btn").onclick = () => {
   document.body.classList.toggle("dark-mode");
 };
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
