81f3bc24c68e57deae9e35fad72c497a6a0fd96a
raunak5131 <raunakvernekar19@gmail.com>
2025-06-22 23:31:27 +0530

    Made navbar responsive and added toggle menu JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("toggle-btn").onclick = () => {
   document.body.classList.toggle("dark-mode");
 };
-function toggleMenu() {
-  document.getElementById("navLinks").classList.toggle("active");
-}
+
+document.getElementById("menu-toggle").onclick = () => {
+  document.getElementById("nav-links").classList.toggle("active");
+};
